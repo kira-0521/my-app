ef66ee50d8ee65f4e2f302881a554c634b3cbccd
kira-0521 <kiratanaka0521@gmail.com>
2022-06-09 19:41:52 +0900

    [ add ] path alias

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Layout from '../components/layouts/Layout'
+import styles from '@/styles/Home.module.css'
+import Layout from '@/components/layouts/Layout'
 
 const Home: NextPage = () => {
   return (
