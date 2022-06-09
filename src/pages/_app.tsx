8a90ccf2c1dc237b45b4d585c9e034cc590420ba
kira-0521 <kiratanaka0521@gmail.com>
2022-06-09 23:03:55 +0900

    [ feature ] global theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
+import { ThemeProvider } from '@mui/material'
+
+import { theme } from '../theme'
+
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
